e44e375dba2dc52896beade0388f40c85cd031e3
Saurabh <saurabhkushwaha2030@gmail.com>
2024-03-18 20:25:19 +0530

    change server.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-
+// app.js
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import jobRouter from "./routes/jobRoutes.js";
@@ -13,31 +13,29 @@ import fileUpload from "express-fileupload";
 const app = express();
 config({ path: "./config/config.env" });
 
-
-
+// Middleware
 app.use(cors({
   origin: ["http://localhost:5173", "https://job-portal-app-zlg3.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true // Allow credentials (cookies, authorization headers, etc.)
 }));
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp/",
-  })
-);
+// Routes
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 
+// Enable pre-flight OPTIONS request for CORS
 app.options('*', cors());
 
+// Error middleware
+app.use(errorMiddleware);
+
+// Database connection
 dbConnection();
 
-app.use(errorMiddleware);
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
+// server.js
 import path from "path";
 import express from "express";
-import app from "./app.js"; // Assuming app.js exports the Express app
+import app from "./app.js"; // Import the Express app from app.js
+import cloudinary from "cloudinary"; // Import cloudinary here
 
+// Cloudinary configuration
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
+
+// Define the directory path
 const __dirname = path.resolve();
 
 // Serve static files
@@ -12,18 +22,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-// Cloudinary configuration
-import cloudinary from "cloudinary";
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-  api_key: process.env.CLOUDINARY_CLIENT_API,
-  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
-});
-
 // Start the server
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
-
-
 
